acf10fc827492e9cb5073fe58a3e2848d4c6ec1c
Kristiyan Balev <kristiyan.balev@gmail.com>
2023-04-05 19:28:39 +0200

    fixing folder structure, prisma and turborepo

---
+++ apps/post-service/src/index.ts
@@ -0,0 +1,11 @@
+import { createServer } from "./server";
+import { postRouter } from "./routes";
+
+const port = process.env.NEXT_PUBLIC_POST_SERVICE_URL || 3003;
+const server = createServer();
+
+server.listen(port, () => {
+    console.log(`Post service listening on port ${port}!`)
+});
+
+server.use("/post", postRouter());
